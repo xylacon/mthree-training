6ba09f73c56dff28ce3fd8e6ad70236b7ad62815
Richard Denton <xylacon@gmail.com>
2025-07-22 16:59:15 -0500

    Finished implementation of tic_tac_toe

--- tic_tac_toe/main.cpp
+++ tic_tac_toe/main.cpp
@@ -99,70 +99,127 @@ void printBoard(int** board, const int rows, const int cols) {
 	}
 }
 
+bool notMatch(const int curr, const int prev) {
+	return (prev == 0 || curr != prev);
+}
+
 int checkBoard(int** board, const int rows, const int cols) {
+	// Inefficient but easy to implement
+	int prev;
+	bool found;
+
+	// Rows
+	for (int i(0); i < rows; ++i) {
+		prev = board[i][0];
+		found = true;
+		for (int j(1); j < cols; ++j)
+			if (notMatch(board[i][j], prev))
+				found = false;
+
+		if (found)
+			return prev;
+	}
+
+	// Columns
+	for (int i(0); i < cols; ++i) {
+		prev = board[0][i];
+		found = true;
+		for (int j(1); j < rows; ++j)
+			if (notMatch(board[j][i], prev))
+				found = false;
+
+		if (found)
+			return prev;
+	}
+
+	// Diagonals
+	prev = board[0][0];
+	found = true;
+	for (int i(1), j(1); i < rows && j < cols; ++i, ++j) {
+		if (notMatch(board[i][j], prev))
+			found = false;
+	}
+	if (found)
+		return prev;
 	
+	prev = board[rows-1][0];
+	found = true;
+	for (int i(rows-2), j(1); i >= 0 && j < cols; --i, ++j) {
+		if (notMatch(board[i][j], prev))
+			found = false;
+	}
+	if (found)
+		return prev;
+	
+	// Check if full
+	int countEmpty(0);
+	for (int i(0); i < rows; ++i)
+		for (int j(0); j < cols; ++j)
+			if (board[i][j] == 0)
+				++countEmpty;
+	
+	if (countEmpty == rows * cols)
+		return 0;
 
 	return -1;
 }
 
 int playGame(int** board, const int rows, const int cols) {
 	int winner(-1);
+	bool userTurn(false);
+	string input;
 	do {
-		bool userTurn(false);
-		string input;
+		// User: 1
+		// CPU: 2
+		const int player( userTurn ? 1 : 2);
+		int row(-1), col(-1);
+
+		// CPU turn
+		if (!userTurn) {
+			srand(time(0));
+			int row, col;
+			do {
+				row = rand() % 3;
+				col = rand() % 3;
+			} while (!updateBoard(board, rows, cols, player, row, col));
+
+			userTurn = true;
+			winner = checkBoard(board, rows, cols);
+			continue;
+		}
+
+		// Player turn
+		printBoard(board, rows, cols);
 		while (true) {
-			// User: 1
-			// CPU: 2
-			const int player( userTurn ? 1 : 2);
-			int row(-1), col(-1);
+			cout << "Enter your move: ";
+			getline(cin, input);
 
-			// CPU turn
-			if (!userTurn) {
-				srand(time(0));
-				int row, col;
-				do {
-					row = rand() % 3;
-					col = rand() % 3;
-				} while (!updateBoard(board, rows, cols, player, row, col));
-
-				userTurn = true;
+			if (input.length() != 2) {
+				cout << "Invalid move.\n\n";
 				continue;
 			}
 
-			// Player turn
-			printBoard(board, rows, cols);
-			while (true) {
-				cout << "Enter your move: ";
-				getline(cin, input);
-
-				if (input.length() != 2) {
-					cout << "Invalid move.\n\n";
-					continue;
-				}
-
-				if (isalpha(input[0]) && isdigit(input[1])) {
-					row = input[0] - 'A';
-					col = input[1] - '1';
-				}
-				else if (isdigit(input[0]) && isalpha(input[1])) {
-					row = input[1] - 'A';
-					col = input[0] - '1';
-				}
-				else {
-					cout << "Invalid move.\n\n";
-					continue;
-				}
-
-				if (updateBoard(board, rows, cols, player, row, col))
-					break;
-				
-				cout << "Invalid move.\n\n";
+			if (isalpha(input[0]) && isdigit(input[1])) {
+				row = toupper(input[0]) - 'A';
+				col = input[1] - '1';
 			}
-			cout << endl;
-			
-			userTurn = false;
-		}
+			else if (isdigit(input[0]) && isalpha(input[1])) {
+				row = toupper(input[1]) - 'A';
+				col = input[0] - '1';
+			}
+			else {
+				cout << "Invalid move.\n\n";
+				continue;
+			}
 
+			if (updateBoard(board, rows, cols, player, row, col))
+				break;
+			
+			cout << "Invalid move.\n\n";
+		}
+		cout << endl;
+
+		userTurn = false;
 		winner = checkBoard(board, rows, cols);
 	} while(winner == -1);
 
@@ -180,16 +237,19 @@ void run() {
 				cout << "Tie";
 				break;
 			case 1:
-				cout << "CPU wins";
+				cout << "You win";
 				break;
 			case 2:
-				cout << "You win";
+				cout << "CPU wins";
 				break;
 			default:
 				cout << "ERROR";
 		}
-
 		cout << "\n\n";
+		printBoard(board, rows, cols);
+		cout << "\n\n";
+
+		deleteBoard(board, rows);
 	}
 }
 
